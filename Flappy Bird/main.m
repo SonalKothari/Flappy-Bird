e2a55832a2e8c4e9c5377574ea13d96c7ce31799
Mehta <casa@mehtas-macbook.local>
2016-01-29 17:11:47 +0530

    Initial Commit

---
+++ Flappy Bird/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Flappy Bird
+//
+//  Created by Mehta on 29/01/16.
+//  Copyright © 2016 Mehta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
